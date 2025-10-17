dd83cfedc37f82c73a48345258377633749da4c3
dossonang <179678372+DossonAng@users.noreply.github.com>
2025-10-17 09:16:55 +0800

    Complete JavaScript exercise "07_reverseString"

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let arr = string.split('');
 
+  let reversedArr = arr.reverse();
+  let joinedReversedArr = reversedArr.join('');
+
+  return joinedReversedArr;
 };
 
 // Do not edit below this line
