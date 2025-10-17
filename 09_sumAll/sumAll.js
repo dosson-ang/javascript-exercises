4c5de94a4eff8a42dfdb917875535d1653617008
dossonang <179678372+DossonAng@users.noreply.github.com>
2025-10-17 14:17:34 +0800

    Complete JavaScript exercise "09_sumAll"

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(positiveInt, secondPositiveInt) {
+  let smallestPositiveInt = Math.min(positiveInt, secondPositiveInt);
+  let biggestPositiveInt = Math.max(positiveInt, secondPositiveInt);
+  let sumOfPositiveIntegers = 0;
 
+  if (smallestPositiveInt >= 0 && 
+    biggestPositiveInt >= 0 && 
+    typeof smallestPositiveInt === 'number' && 
+    typeof biggestPositiveInt === 'number' &&
+    Number.isInteger(positiveInt) &&
+    Number.isInteger(secondPositiveInt)) {
+    for (let i = smallestPositiveInt; i <= biggestPositiveInt; i++) {
+      sumOfPositiveIntegers += i;
+    }
+
+    return sumOfPositiveIntegers;
+  } else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
